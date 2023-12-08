c45ad7366a55d69a3851a2e3f5bbfe76dfd065ab
Pedro Aruã Veloso <pedro.veloso@capgemini.com>
2023-12-08 08:11:32 -0300

    retirada do login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './modules/base/login/login.component';
 
-const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/base/login/login.component';
-import { TopMenuComponent } from './modules/menu/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    TopMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
